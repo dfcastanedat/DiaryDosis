48126818ef75990d06352e07e077e79bb6767bda
Daniel Castaneda <dfcastanedat@unal.edu.co>
2024-03-14 11:52:17 -0500

    First commit

---
+++ iwiSpectate.py
@@ -0,0 +1,2 @@
+# 1. http request to /lol/spectator/v5/active-games/by-summoner/{encryptedPUUID} using puuid hlpuNrEuoN6SijjQnbi3Qww7Jj75stvLHSd4XwVTlNjzXnTkvJe9v4AutMbim55G9jptzWz2HTA5Kw
+# 2. Edit and run .bat script using mentioned puuid and game encryptionKey
